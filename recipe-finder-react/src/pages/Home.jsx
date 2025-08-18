d687873bc0b75fbda6c5dd7e92b623f0bb3d7eb8
Marcela <mahmoreti@hotmail.com>
2025-08-18 14:41:03 +0100

    section 2 and 3

--- recipe-finder-react/src/pages/Home.jsx
+++ recipe-finder-react/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import fuss from "../assets/images/icon-minimum-fuss.svg";
 import search from "../assets/images/icon-search-in-seconds.svg";
 import imagehomeherosmall from "../assets/images/image-home-hero-small.webp";
 import patternsquiggle1 from "../assets/images/pattern-squiggle-1.svg";
+import imagehomereallifesmall from "../assets/images/image-home-real-life-small.webp";
 
 const Home = () => {
   return (
@@ -14,8 +15,8 @@ const Home = () => {
         <img className="backgroundIcon" src={patternsquiggle1} alt="patternsquiggle1" />
         <main className='mainHome'>
         <section className='section1'>
-            <h1>Healthy meals, zero fuss</h1>
-            <p>
+            <h1 className='h1'>Healthy meals, zero fuss</h1>
+            <p className='p'>
             Discover eight quick, whole-food recipes that you can cook tonight—no
             processed junk, no guesswork.
             </p>
@@ -27,33 +28,42 @@ const Home = () => {
         </section>
 
         <section className='section2'>
-            <h2>What you'll get</h2>
-            <div>
-                <img src={carrot} alt="Carrots" />
-                <h3>Whole-food recipes</h3>
-                <p>Each dish uses everyday, unprocessed ingredients.</p>
-
-                <img src={fuss} alt="Fuss" />
-                <h3>Minimum fuss</h3>
-                <p>All recipes are designed to make eating healthy quick and easy.</p>
-
-                <img src={search} alt="Search" />
-                <h3>Search in seconds</h3>
-                <p>Filter by name or ingredient and jump straight to the recipe you need.</p>
+            <h2 className='h2'>What you'll get</h2>
+            <div className='divSection2'>
+                <div className='blockSection2' >
+                    <img src={carrot} alt="Carrots" />
+                    <h3 className='h3'>Whole-food recipes</h3>
+                    <p className='p'>Each dish uses everyday, unprocessed ingredients.</p>
+                </div>
+                <div className='blockSection2'>    
+                    <img src={fuss} alt="Fuss" />
+                    <h3 className='h3'>Minimum fuss</h3>
+                    <p className='p'>All recipes are designed to make eating healthy quick and easy.</p>
+                </div>    
+                <div className='blockSection2'>
+                    <img src={search} alt="Search" />
+                    <h3 className='h3'>Search in seconds</h3>
+                    <p className='p'>Filter by name or ingredient and jump straight to the recipe you need.</p>
+                </div>
             </div>
         </section>
 
-        <section>
-            <h2>Built for real life</h2>
-            <p>
+        <section className='section3'>
+            <h2 className='h2'>Built for real life</h2>
+            <p className='p'>
             Cooking shouldn't be complicated. These recipes come in under 30
             minutes of active time, fit busy schedules, and taste good enough to
             repeat.
             </p>
-            <p>
+            <p className='p'>
             Whether you're new to the kitchen or just need fresh ideas, we've got
             you covered.
             </p>
+
+            <div className='imgPhotoSthree'>
+                <img className="photo2" src={imagehomereallifesmall} alt="imagehomereallifesmall" />
+            </div>    
+
         </section>
 
         <section>
